36a951a38f006eeff0db8f495118fcc4fc19b8c8
avifriedman <130413078+avifriedman@users.noreply.github.com>
2025-10-03 04:27:56 +0300

    Update and rename ai_studio_code.js to main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const config = {
     synagogueName: "קהילת ידידיה",
 
     // הדבק כאן את הקישור לקובץ ה-CSV שפרסמת מגוגל שיטס
-    googleSheetUrl: "הדבק כאן את הקישור שלך"
-};
+    googleSheetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7e8R5Z3x5QG63ZU51Y9BF9eUdDsYpcaPJHgwnRc9U86TwgUcMhVLbWwGigKoGb3e8lxMCCvJHrzhy/pubhtml
+        
